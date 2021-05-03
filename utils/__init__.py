c7337a350022c54637313ef33102c320441b6a10
peviroy <peviroy@outlook.com>
2021-05-03 21:44:04 +0800

    added(yolo_gt_generator): temp placed

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
-from .util import draw_acc_loss
+from .util import *
 from .torch_util import accuracy_calc
 from .torch_util import adjust_learning_rate
--- utils/util.py
+++ utils/util.py
@@ -3,8 +3,67 @@ import numpy as np
 import cv2
 from numpy import ndarray
 import matplotlib.pyplot as plt
+import torch
 """ Generic API
 """
+
+
+def generate_dxdywh(gt_label, w, h, s):
+    xmin, ymin, xmax, ymax = gt_label[:-1]
+    # compute the center, width and height
+    c_x = (xmax + xmin) / 2 * w
+    c_y = (ymax + ymin) / 2 * h
+    box_w = (xmax - xmin) * w
+    box_h = (ymax - ymin) * h
+
+    if box_w < 1e-4 or box_h < 1e-4:
+        # print('A dirty data !!!')
+        return False
+
+    # map center point of box to the grid cell
+    c_x_s = c_x / s
+    c_y_s = c_y / s
+    grid_x = int(c_x_s)
+    grid_y = int(c_y_s)
+    # compute the (x, y, w, h) for the corresponding grid cell
+    tx = c_x_s - grid_x
+    ty = c_y_s - grid_y
+    tw = np.log(box_w)
+    th = np.log(box_h)
+    weight = 2.0 - (box_w / w) * (box_h / h)
+
+    return grid_x, grid_y, tx, ty, tw, th, weight
+
+
+def gt_creator(input_size, stride, label_lists=[]):
+    # prepare the all empty gt datas
+    batch_size = len(label_lists)
+    w = input_size
+    h = input_size
+
+    # We  make gt labels by anchor-free method and anchor-based method.
+    ws = w // stride
+    hs = h // stride
+    s = stride
+    gt_tensor = np.zeros([batch_size, hs, ws, 1 + 1 + 4 + 1])
+
+    # generate gt whose style is yolo-v1
+    for batch_index in range(batch_size):
+        for gt_label in label_lists[batch_index]:
+            gt_class = int(gt_label[-1])
+            result = generate_dxdywh(gt_label, w, h, s)
+            if result:
+                grid_x, grid_y, tx, ty, tw, th, weight = result
+
+                if grid_x < gt_tensor.shape[2] and grid_y < gt_tensor.shape[1]:
+                    gt_tensor[batch_index, grid_y, grid_x, 0] = 1.0
+                    gt_tensor[batch_index, grid_y, grid_x, 1] = gt_class
+                    gt_tensor[batch_index, grid_y, grid_x, 2:6] = np.array([tx, ty, tw, th])
+                    gt_tensor[batch_index, grid_y, grid_x, 6] = weight
+
+    gt_tensor = gt_tensor.reshape(batch_size, -1, 1 + 1 + 4 + 1)
+
+    return torch.from_numpy(gt_tensor).float()
 
 
 def get_image_paths(directory: str) -> list:
@@ -39,9 +98,9 @@ def load_images(image_paths: list, image_size=(256, 256)) -> ndarray:
         list of images in the form of numpy array
     '''
     iter_all_images = (cv2.resize(cv2.imread(image_path), image_size) for image_path in image_paths)
+    image = iter_all_images[0]
+    all_images = np.empty((len(image), ) + image.shape, dtype=image.dtype)
     for i, image in enumerate(iter_all_images):
-        if i == 0:
-            all_images = np.empty((len(image), ) + image.shape, dtype=image.dtype)
         all_images[i] = image
     return all_images
 
