df1ab0f5dcebc2ed32cab2a2930bc2f43a5d21ec
peviroy <peviroy@outlook.com>
2021-05-04 17:29:13 +0800

    fix: wrong parameter passing!

--- nn/modules.py
+++ nn/modules.py
@@ -4,7 +4,7 @@ from torch import nn
 
 
 class Conv2d(nn.Module):
-    def __init__(self, in_c, out_c, k_size, padding=0, stride=1, leakyReLU=False):
+    def __init__(self, in_c, out_c, k_size, stride=1, padding=0, leakyReLU=False):
         super(Conv2d, self).__init__()
         self.conv = nn.Conv2d(in_c, out_c, k_size, stride, padding)
         self.bn = nn.BatchNorm2d(out_c)
@@ -18,10 +18,10 @@ class SpatialPyramidPool2d(nn.Module):
     def __init__(self, in_channels, out_channels, k=(5, 9, 13)):
         super(SpatialPyramidPool2d, self).__init__()
         planes = in_channels // 2 # hidden channels
-        self.cv1 = Conv2d(in_channels, planes, 1, 1)
+        self.cv1 = Conv2d(in_channels, planes, k_size=1, stride=1)
         self.pools = nn.ModuleList(
             [nn.MaxPool2d(kernel_size=x, stride=1, padding=x // 2) for x in k])
-        self.cv2 = Conv2d(planes * (len(k) + 1), out_channels, 1, 1)
+        self.cv2 = Conv2d(planes * (len(k) + 1), out_channels, k_size=1, stride=1)
 
     def forward(self, x):
         x = self.cv1(x)
